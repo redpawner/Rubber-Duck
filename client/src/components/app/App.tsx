bac3ce57fb5f7b11952bfb700c85443f29b59f93
Rob4ert <rob.ryb4@gmail.com>
2022-06-23 18:00:48 +0100

    beta dashboard

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Dashboard from './dashboard/dashboard';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className='container'>
+    <Dashboard />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/app/dashboard/dashboard.tsx
+++ client/src/components/app/dashboard/dashboard.tsx
@@ -1,1 +1,16 @@
 import React from "react";
+import "./dashboard.css"
+
+export default function Dashboard () {
+  return (
+    <div className="dashboard-container">
+      <div className="create-help-butt-div">
+        <button className="create-help-butt"> <a href="https://www.youtube.com/watch?v=4vvBAONkYwI">CREATE HELP REQUEST</a></button>
+      </div>
+        <ul className="help-list">
+          <li>HELP ME</li>
+          <li> HELP ME</li>
+        </ul>
+    </div>
+  )
+}
