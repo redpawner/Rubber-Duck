7c773af213e4e71d7b65b7a6fb8626f9cc6f09fe
Rob4ert <rob.ryb4@gmail.com>
2022-06-23 16:36:52 +0100

    navbar

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -1,1 +1,19 @@
 import React from "react";
+import "./navbar.css"
+
+export default function Navbar () {
+  return (
+    <div className="navbar">
+     <div className="small-logo">
+      <img className="logo-img" alt="logo"></img>
+      </div>
+      <div className="buttons-container">
+       <button className="logout">logout</button>
+      <button className="button2">Another button</button> 
+      </div>
+    </div> 
+
+   
+    
+  )
+}
