8edbefaf3ee59fc034590f5f5f239813263ada2e
Rob4ert <rob.ryb4@gmail.com>
2022-06-24 19:42:55 +0100

    css applied

--- client/src/components/app/dashboard/create-help-request/create-help-request.tsx
+++ client/src/components/app/dashboard/create-help-request/create-help-request.tsx
@@ -5,9 +5,9 @@ export default function CreateHelp() {
   return (
     <div className="help-req-container">
       <img
-        className="bread"
-        src="https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2FPhoto%2FRecipe%20Ramp%20Up%2F2021-11-Potato-Bread%2Fpotato_bread_01"
-        alt="bread"
+        className="weird"
+        src="http://static.demilked.com/wp-content/uploads/2020/09/5f6067aedb19e-awkward-funny-family-photos-19-5f5a114e65ee3__700.jpg"
+        alt="wtf"
       ></img>
     </div>
   );
