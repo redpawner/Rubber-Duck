c405de288681902b8c66a304826718239d98ed4c
TkRj <tekraj84@hotmail.com>
2022-06-24 23:25:30 +0100

    working out TS for schema

--- server/models/schema.ts
+++ server/models/schema.ts
@@ -1,14 +1,54 @@
-import mongoose from 'mongoose';
-import conn from './db';
+import { Schema, model } from "mongoose";
+import conn from "./db";
 
-const { Schema } = mongoose;
+// const User = new Schema({
+//   email: { type: String, required: true },
+//   username: { type: String, required: true },
+//   user_languages: [{ type: String, required: true }],
+// });
 
-const User = new Schema({
-  email: { type: String, required: true },
-  username: { type: String, required: true },
-  user_languages: [{ type: String, required: true }],
-});
+// 1. Create an interface representing a document in MongoDB.
+interface User {
+  username: string;
+  email: string;
+  avatar?: string;
+  user_languages: string[];
+  rating_total: Number;
+  rating_count: Number;
+  needHelp: Boolean;
+  help_request: HelpReqSchema;
+}
 
-const Users = conn.model('Users', User);
+interface HelpReqSchema {
+  username: string;
+  title: string;
+  description: string;
+  hr_languages: string[];
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<User>(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    avatar: String,
+    user_languages: [
+      {
+        type: String,
+        enum: {
+          values: ["Javascript", "Java", "PHP", "C#"],
+          message: "No language selected.",
+        },
+      }
+    ],
+    rating_total: { type: Number, default: 0 },
+    rating_count: { type: Number, default: 0 },
+    needHelp: { type: Boolean, default: false },
+    help_request: { type: HelpReqSchema, default: {} },
+  },
+  { timestamps: true }
+);
+
+const Users = conn.model("User", userSchema);
 
 module.exports = Users;
