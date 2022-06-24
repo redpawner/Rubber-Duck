7c28110bdd842200683cf004dc34464474bb8a9f
Rob4ert <rob.ryb4@gmail.com>
2022-06-24 17:48:46 +0100

    sass done

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
-import useStore from '../../state-stores/state-stores';
-import './App.css';
-import CreateHelp from './dashboard/create-help-request/create-help-request';
-import Dashboard from './dashboard/dashboard';
-
-
+import React from "react";
+import useStore from "../../state-stores/state-stores";
+import "./App.scss";
+import CreateHelp from "./dashboard/create-help-request/create-help-request";
+import Dashboard from "./dashboard/dashboard";
 
 function App() {
-
-  const bears = useStore((state) => state.show)
+  const bears = useStore((state) => state.show);
 
   return (
-    <div className='container'>
-    {bears ? <Dashboard /> : < CreateHelp />}
-    </div>
+    <div className="container">{!bears ? <Dashboard /> : <CreateHelp />}</div>
   );
 }
 
