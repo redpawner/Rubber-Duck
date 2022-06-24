96827f12c8ecad9c036fac019f753ec5dad9842b
redpawner <raymondspence@hotmail.com>
2022-06-24 17:41:54 +0100

    added mongodb

---
+++ server/models/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const SERVER = process.env.SERVER;
+
+console.log('test');
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://' + SERVER);
+}
+
+let conn = mongoose.connection;
+conn.on('connected', function () {
+  console.log('Database connected successfully.');
+});
+conn.on('disconnected', function () {
+  console.log('Database disconnected successfully.');
+});
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+module.exports = conn;
