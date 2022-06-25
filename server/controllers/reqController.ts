2f35cd66d9e49805ad86ddd3de71b150eaa07f4e
redpawner <raymondspence@hotmail.com>
2022-06-25 12:08:38 +0100

    for tekraj

---
+++ server/controllers/reqController.ts
@@ -0,0 +1,3 @@
+const reqController = {};
+
+export default reqController;
--- server/index.ts
+++ server/index.ts
@@ -3,28 +3,52 @@ import parser from 'koa-bodyparser';
 import cors from '@koa/cors';
 import router from './router';
 import dotenv from 'dotenv';
+import { ApolloServer } from 'apollo-server-koa';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import http from 'http';
 
 dotenv.config({ path: './.env' });
 
-const conn = require('./models/db');
-
-const App = new Koa();
+const conn = require('./dbmodels/db');
 const PORT = process.env.PORT || 3002;
 
-App.use(cors());
-App.use(parser());
-App.use(router.routes());
+(async (typeDefs, resolvers) => {
+  const httpServer = http.createServer();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-App.use(async (ctx: any, next: any) => {
-  await next();
-  if (ctx.status === 404) {
-    ctx.body = '_404';
-    ctx.status = 404;
-  }
-});
+  await server.start();
+  const app = new Koa();
 
-(async () => {
-  App.listen(PORT, () =>
-    console.log(`🚀 Server running at http://127.0.0.1:${PORT}/ 🚀`)
+  app.use(cors());
+  app.use(parser());
+  app.use(router.routes());
+
+  app.use(async (ctx: any, next: any) => {
+    await next();
+    if (ctx.status === 404) {
+      ctx.body = '_404';
+      ctx.status = 404;
+    }
+  });
+  server.applyMiddleware({ app });
+  httpServer.on('request', app.callback());
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: PORT }, resolve)
   );
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
+  return { server, app };
 })();
+
+// (async () => {
+//   App.listen(PORT, () =>
+//     console.log(`🚀 Server running at http://127.0.0.1:${PORT}/ 🚀`)
+//   );
+// })();
