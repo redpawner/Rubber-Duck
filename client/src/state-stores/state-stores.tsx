efcd0024d4a92331be1d17f69e6d3a8b73aa561c
Alicia Trujillo Samper <alitrusam@gmail.com>
2022-06-25 10:46:56 +0200

    FE state-stores middleware added

--- client/src/state-stores/state-stores.tsx
+++ client/src/state-stores/state-stores.tsx
@@ -1,17 +1,19 @@
 import react from "react";
 import create from "zustand";
+//Enable redux devtools in browser
+import { devtools } from "zustand/middleware"
 
-// NEEDED OTHERWISE TypeScript complains 
-type MyStore = {   
+// NEEDED OTHERWISE TypeScript complains
+type MyStore = {
   show: boolean;
   toggleShow: () => void;
 };
 
 
-const useStore = create<MyStore>(set => ({
+const useStore = create<MyStore>()(devtools(set => ({
   show: false,
   toggleShow: () => set((state) => ({ show: !state.show }))
-}));
+})));
 
 
 export default useStore;
