af46030141a86254a73f6adee3333bf5f5f33f19
Rob4ert <rob.ryb4@gmail.com>
2022-06-29 13:42:10 +0100

    search

--- client/src/components/app/dashboard/dashboard.tsx
+++ client/src/components/app/dashboard/dashboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./dashboard.scss";
 import { buttonsLogicStore } from "../../../state-stores/state-stores";
 import Help from "./help-request/help-request";
@@ -6,14 +7,46 @@ import Help from "./help-request/help-request";
 export default function Dashboard() {
   const helpDash = buttonsLogicStore((state) => state.toggleShow);
 
+  const [formValue, setFormValue] = useState("");
+
+  useEffect(() => {
+    console.log(formValue);
+  }, [formValue]);
+
+  const handleChange = (e: any) => {
+    setFormValue(e.target.value);
+  };
+
   return (
     <div className="dashboard-container">
-      <div className="create-help-butt-div">
-        <h1 className="dashboard-title">Help Requests</h1>
-        <button className="create-help-butt" onClick={helpDash}>
-          <i className="fa fa-plus"></i> &nbsp; Create
-        </button>
+      <div className="flexcolumn">
+        <div className="create-help-butt-div">
+          <h1 className="dashboard-title">Help Requests</h1>
+          <button className="create-help-butt" onClick={helpDash}>
+            <i className="fa fa-plus"></i> &nbsp; Create
+          </button>
+        </div>
+        <div className="search-me-officer">
+          <form
+            className="search-field"
+            onSubmit={(e) => {
+              e.preventDefault(); //this stops it loading URL with the name value
+            }}
+          >
+            <input
+              type="text"
+              // onChange={handleChange}
+              name="search"
+              autoComplete="off"
+              placeholder="Filter..."
+            />
+          </form>
+          <ul className="search-tags">
+            <li>CSS</li>
+          </ul>
+        </div>
       </div>
+
       <ul className="help-list">
         <li>
           <Help />
